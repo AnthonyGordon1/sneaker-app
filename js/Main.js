f295274ba52de98cfd04bdc081228a5e528940b0
AnthonyGordon1 <anthonygordonunt@gmail.com>
2017-04-02 21:02:05 -0700

    Card Object Created, Minor Changes

--- js/Main.js
+++ js/Main.js
@@ -1,1 +1,17 @@
 // Main JavaScript Document
+var cards = [];
+var mainContent = document.getElementId('main-content');
+var mainContentRegion = new ZingTouch.Region(mainContent);
+
+//Create the card object template
+function contentCard(title, img, desc, link) {
+		"use strict";
+		this.title = title;
+		this.img   = img;
+		this.desc  = desc;
+		this.link  = link;
+}
+
+var cardPicker = function() {
+	
+};
