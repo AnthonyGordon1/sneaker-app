3f594741d55735c20ae1b7f083222e9d10e35fb5
AnthonyGordon1 <anthonygordonunt@gmail.com>
2017-04-02 21:56:24 -0700

    Create Card Object template and Card Picker

--- js/Main.js
+++ js/Main.js
@@ -5,13 +5,31 @@ var mainContentRegion = new ZingTouch.Region(mainContent);
 
 //Create the card object template
 function contentCard(title, img, desc, link) {
-		"use strict";
-		this.title = title;
-		this.img   = img;
-		this.desc  = desc;
-		this.link  = link;
+	"use strict";
+	this.title = title;
+	this.img   = img;
+	this.desc  = desc;
+	this.link  = link;
 }
 
-var cardPicker = function() {
+var cardPicker = (function() {
+	"use strict";
+	var cards = [];
+	var index = 0;
 	
-};
+	function next() {
+		index = index++ < cards.length ? index : 0;
+		return cards(index);
+	}
+	
+	function current() {
+		return cards(index);
+	}
+	
+	return {
+		next: next,
+		current: current
+	};
+	
+	
+})();
